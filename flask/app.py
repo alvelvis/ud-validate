1ad4523376f00ebb14d9cc884b1445491bd40f9b
Elvis de Souza <elvis.desouza99@gmail.com>
2022-06-11 21:19:07 -0300

    access count introduced

--- flask/app.py
+++ flask/app.py
@@ -1,36 +1,46 @@
 from flask import Flask, redirect, render_template, request
 import os
-import sys
-import shutil
-import re
+import json
 
 app = Flask(__name__)
+app_path = os.path.dirname(os.path.abspath(__file__))
+config_path = os.path.join(app_path, "config.json")
+validate_path = os.path.join(os.path.dirname(app_path), "tools", "validate.py")
+sentence_path = os.path.join(app_path, "sentence.conllu")
 
-@app.route("/validate", methods="POST GET".split())
-def validate():
-    return redirect("/")
+def save_config():
+    with open(config_path, "w") as f:
+        json.dump(config, f)
+
+def increase_access_number(n=1):
+    config['access_number'] += n
+    save_config()
+
+# load config
+if not os.path.isfile(config_path):
+    config = {'access_number': 1}
+    save_config()
+else:
+    with open(config_path) as f:
+        config = json.load(f)
 
 @app.route('/', methods="POST GET".split())
-def home():
-    conllu = ""
-    validation = ""
-    update = ""
-    
-    # POST
-    if 'inputText' in request.values:
+def home(conllu="", validation="", update=""):
+    if request.method == "POST":
+        # convert new-line to linux style and add empty line in the end (validation requirements)
         conllu = request.values.get("inputText").strip().replace("\r\n", "\n") + "\n\n"
-        with open("sentence.conllu", "w") as f:
+        with open(sentence_path, "w") as f:
             f.write(conllu)
         try:
-            validation = os.popen("python3 '{}/tools/validate.py' --max-err=0 --lang=pt sentence.conllu 2>&1".format(
-            os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
+            validation = os.popen("python3 '{}' --max-err=0 --lang=pt '{}' 2>&1".format(
+                validate_path,
+                sentence_path
             ), "r").read()
         except Exception as e:
             validation = str(e)
-        os.remove("sentence.conllu")
-    
-    # GET
-    else:
+        os.remove(sentence_path)
+        increase_access_number(conllu.count("\n\n"))    
+    elif request.method == "GET":
         os.popen("git pull")
         update = os.popen("git pull --recurse-submodules 2>&1", "r").read()
     return render_template(
@@ -39,4 +49,9 @@ def home():
         conllu=conllu.strip(),
         validation=validation,
         update=update,
+        access_number=config['access_number']
         )
+
+@app.route("/validate", methods="POST GET".split())
+def validate():
+    return redirect("/")
